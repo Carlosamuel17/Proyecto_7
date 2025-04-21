413278fe8a6d19c440fae21301215ee74f7dfa26
Carlosamuel <carlosamuel417@gmail.com>
2025-04-21 14:43:29 -0600

    Creacion de configuracion para Render

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') 
-hist_button = st.button('Construir histograma') # crear un botón
+
 build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram: # al hacer clic en el botón
-    # escribir un mensaje
+if build_histogram: # al hacer clic en el botón se crea un mensaje y el histograma interactivo
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    # crear un histograma
     fig = px.histogram(car_data, x="odometer")    
-    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
