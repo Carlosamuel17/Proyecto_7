f9ded356234c7774f89f1c5ef99bf10064e727eb
Carlosamuel <carlosamuel417@gmail.com>
2025-04-21 19:55:06 -0600

    Dar los toques finales a la aplicación para que funcione bien localmente

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-import pandas as pd
-import plotly.express as px
-import streamlit as st
+import pandas as pd # type: ignore
+import plotly.express as px # type: ignore
+import streamlit as st # type: ignore
 
 car_data = pd.read_csv('vehicles_us.csv') 
 
-build_histogram = st.checkbox('Construir un histograma')
+st.title('Datos de anuncios de coches')
+st.text('En esta página web podremos analizar el dataframe sobre \nanuncios de coches, contiene la siguiente información:')
+
+st.dataframe(car_data)
+
+build_histogram = st.checkbox('Construir histograma de distancia recorrida')
 
 if build_histogram: # al hacer clic en el botón se crea un mensaje y el histograma interactivo
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="odometer")    
+    st.write('Histograma de distancia recorrida por el coche')
+    fig = px.histogram(car_data, x="odometer", )    
     st.plotly_chart(fig, use_container_width=True)
+
+build_scatter = st.checkbox('Construir un gráfico de dispersión de precio vs distancia recorrida')
+
+if build_scatter: # al hacer clic en el botón se crea un mensaje y el gráfico de dispersión interactivo
+    st.write('Gráfico de disperción de precio vs distancia recorrida')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
+
